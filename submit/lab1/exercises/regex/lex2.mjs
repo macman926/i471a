7581f0109ae5711209824344ad96ef3c77484907
Christian Mcborrough <christianmcborrough@gmail.com>
2022-02-03 20:11:35 -0500

    Finished first part of ex1, got partial functionality for second part of ex1

---
+++ submit/lab1/exercises/regex/lex2.mjs
@@ -0,0 +1,7 @@
+export default {
+  $Ignore: /\s+/,  //this will be ignored.
+  ID: /^[A-za-z_][\w]*/,  
+  INT: /\d+/,      //token with kind INT        
+  CHAR: /./,       //single char: must be last;
+                   // /./ is a regex which matches any char other than '\n'
+};
