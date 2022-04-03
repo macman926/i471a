2e6d5c69574874fa647e5771a9cb5852142e3ae9
Christian Mcborrough <christianmcborrough@gmail.com>
2022-04-03 19:50:06 -0400

    Finished all questions

--- submit/prj3/prj3-sol/prj3_sol.pl
+++ submit/prj3/prj3-sol/prj3_sol.pl
@@ -51,8 +51,10 @@ get_greater_than2([H|T], N, M):-
 %Restriction: May not use any auxiliary procedures.
 %Hint: use a rule for empty List and two separate rules for a non-empty List.
 
+get_all_greater_than([], _, []) :- true.
+get_all_greater_than([H|T], N, GTN) :- H > N, get_all_greater_than(T, N, Return), GTN = [H|Return].
+get_all_greater_than([H|T], N, GTN) :- H =< N, get_all_greater_than(T, N, Return), GTN = Return.
 /*********************** get_all_greater_than_tr ***********************/
-
 %% #5 15-points
 %get_all_greater_than_tr(List, N, GreaterThanN): GreaterThanN matches
 %the list of numbers from List (in order) which are greater than N.
@@ -63,6 +65,12 @@ get_greater_than2([H|T], N, M):-
 %Hint: Define an auxiliary procedure witn an extra argument which
 %accumulates GreaterThanN.
 
+helper([], _, GreaterThanN, Temp) :- GreaterThanN = Temp.
+helper([H|T], N, GreaterThanN, Temp) :- H > N, helper(T, N, GreaterThanN, [H|Temp]).
+helper([H|T], N, GreaterThanN, Temp) :- H =< N, helper(T, N, GreaterThanN, Temp).
+get_all_greater_than_tr(List, N, GreaterThanN) :- 
+	helper(List, N, GreaterThanNBackwards, []), 
+	reverse(GreaterThanNBackwards, GreaterThanN).
 /*************************** split_into_pairs **************************/
 
 %% #6 10-points
@@ -73,14 +81,24 @@ get_greater_than2([H|T], N, M):-
 %Restriction: May not use any auxiliary procedures.
 %Hint: consider cases of List empty, List a 1-element list and List
 %containing 2-or-more elements.
-
+split_into_pairs([], []) :- true.
+split_into_pairs([Temp], [[Temp]]) :- true.
+split_into_pairs([First | [Second | Tail]], PairList) :- 
+	PairList = [[First, Second]|Result], split_into_pairs(Tail, Result).
 /******************************** sum_areas ****************************/
 %% #7 10-points
 %sum_areas(Shapes, SumArea): match SumArea to the sum of the shapes in
 %list Shapes.  A shape is either rect(X, Y, W, H) with area W*H or
 %circ(X, Y, R) with area pi * R * R (note pi is defined on the
 %RHS of is/2).
-
+sum_areas([], 0) :- true.
+sum_areas([(rect(_, _, W, H))|Tail], SumArea) :- 
+	Area is W * H, sum_areas(Tail, TailAreas), 
+	SumArea is Area + TailAreas.
+sum_areas([(circ(_, _, R))|Tail], SumArea) :- 
+	Area is pi * R * R, 
+	sum_areas(Tail, TailAreas), 
+	SumArea is Area + TailAreas.
 /******************************* n_prefix ******************************/
 
 %% #8 10-points
@@ -89,7 +107,10 @@ get_greater_than2([H|T], N, M):-
 %length(List) >= N.
 %Restriction: May not use any auxiliary procedures.
 %Hint: Recurse on N.
-
+n_prefix(1, [H|T], [H], Rest) :- Rest = T.
+n_prefix(N, [H|T], Prefix, Rest) :- 
+	Temp is N - 1, n_prefix(Temp, T, P, R), 
+	Prefix = [H|P], Rest = R.
 /***************************** split_into_n_lists **********************/
 
 %% #9 10-points
@@ -100,3 +121,15 @@ get_greater_than2([H|T], N, M):-
 %Hint: List is either empty or non-empty.  If non-empty, its
 %length is < N, or its length is >= N; in the latter case, use n_prefix/3
 %to recurse.
+accumulate(_, [], Return, Accumulator) :- Return = Accumulator.
+accumulate(N, List, Return, Accumulator) :- 
+	length(List, Len), 
+	Len =< N, 
+	Return = [List|Accumulator].
+accumulate(N, List, Return, Accumulator) :- length(List, Len), Len > N,
+	n_prefix(N, List, Head, Tail), 
+	accumulate(N, Tail, Return, 
+	[Head|Accumulator]).
+split_into_n_lists(N, List, NLists) :- 
+	accumulate(N, List, Return, []), 
+	reverse(NLists, Return).
